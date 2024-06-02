7436a4b1797f32b729e73097ee1247c816af827b
Subham Sahu <about.subhamsahu@gmail.com>
2024-06-02 19:54:27 +0530

    .

--- server/server.js
+++ server/server.js
@@ -7,6 +7,15 @@ import mongoose from "mongoose";
 
 const app = express();
 app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 app.use(express.json());
 
 // Connect to MongoDB
