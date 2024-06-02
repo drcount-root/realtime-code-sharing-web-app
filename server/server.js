3bf3bc3713aa3e4572b284987f3293ced0f013d7
Subham Sahu <about.subhamsahu@gmail.com>
2024-06-02 20:06:19 +0530

    .

--- server/server.js
+++ server/server.js
@@ -8,13 +8,9 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+app.use(cors({
+  origin: ["http://localhost:3000", "https://codexyz.vercel.app"]
+}));
 
 app.use(express.json());
 
